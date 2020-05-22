353e862acd79afdbe11e0e0ad5b152ff71646a09
lion <lion_yan@outlook.com>
2020-05-22 19:49:45 +0800

    添加气体传感器

--- co.py
+++ co.py
@@ -2,16 +2,16 @@ from microbit import *
 from enum import *
 
 
-class CO2(object):
+class CO(object):
     """基本描述
 
-    二氧化碳传感器
+    一氧化碳传感器
 
     Args:
         RJ_pin (pin): 连接端口
 
     Returns:
-        value: 二氧化碳含量
+        value: 一氧化碳值
     """
 
     def __init__(self, RJ_pin):
@@ -20,16 +20,16 @@ class CO2(object):
         elif RJ_pin == J2:
             self.__pin = pin2
 
-    def get_co2(self):
+    def get_co(self):
         """基本描述
 
-        获取二氧化碳值
+        获取一氧化碳值
 
         """
         return self.__pin.read_analog()
 
 
 if __name__ == '__main__':
-    co2 = CO2()
+    co = CO()
     while True:
-        print(co2.get_co2())
+        print(co.get_co())
