3a70470a27f848cee8eb97a9ef221b571826a95b
lion <lion_yan@outlook.com>
2021-01-14 10:38:37 +0800

    新增颜色识别返回颜色名函数

--- matrix.py
+++ matrix.py
@@ -65,6 +65,52 @@ class MATRIX(object):
         self.__matBuf[idx + 1] = tmp
         self.__matrix_show()
 
+    def set_matrix_expression(self, expression):
+        """
+
+        显示一个emoji表情
+
+        Args:
+            expression (str): 表情，字符串
+
+        Returns:
+            NONE
+        """
+        if expression == "Neutral":
+            point = [[2, 1], [3, 1], [13, 1], [12, 1],
+                     [2, 2], [3, 2], [13, 2], [12, 2],
+                     [2, 3], [3, 3], [13, 3], [12, 3],
+                     [5, 5], [6, 5], [7, 5], [8, 5], [9, 5], [10, 5],
+                     [5, 6], [6, 6], [7, 6], [8, 6], [9, 6], [10, 6]
+                     ]
+        elif expression == "Sad":
+            point = [[1, 2], [5, 2], [10, 2], [14, 2],
+                     [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [10, 3], [11, 3], [12, 3], [13, 3], [14, 3],
+                     [2, 4], [3, 4], [4, 4], [11, 4], [12, 4], [13, 4],
+                     [6, 6], [7, 6], [8, 6], [9, 6],
+                     [5, 7], [10, 7]
+                     ]
+        elif expression == "Smile":
+            point = [[2, 1], [3, 1], [13, 1], [12, 1],
+                     [2, 2], [3, 2], [13, 2], [12, 2],
+                     [2, 3], [3, 3], [13, 3], [12, 3],
+                     [5, 5], [10, 5],
+                     [6, 6], [7, 6], [8, 6], [9, 6]
+                     ]
+        elif expression == "Angry":
+            point = [[2, 0], [13, 0],
+                     [3, 1], [12, 1],
+                     [3, 2], [4, 2], [11, 2], [12, 2],
+                     [3, 3], [4, 3], [11, 3], [12, 3],
+                     [6, 6], [7, 6], [8, 6], [9, 6],
+                     [5, 7], [10, 7]
+                     ]
+        else:
+            pass
+        self.set_matrix_clear()
+        for i in range(len(point)):
+            self.set_matrix_draw(point[i][0], point[i][1])
+
 
 if __name__ == '__main__':
     dis = MATRIX()
@@ -74,4 +120,8 @@ if __name__ == '__main__':
         for x in range(16):
             dis.set_matrix_draw(x, y)
     dis.set_matrix_clear()
-
+    dis.set_matrix_expression("Angry")
+    sleep(500)
+    dis.set_matrix_expression("Sad")
+    sleep(500)
+    dis.set_matrix_expression("Smile")
